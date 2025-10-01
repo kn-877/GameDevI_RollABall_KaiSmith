8b273e5557438389922a8d88e35f4d96d9c61175
kn-877 <ksm88777@gmail.com>
2025-10-01 10:07:29 -0300

    adjusted ball speed

--- Assets/scripts/playercontroller.cs
+++ Assets/scripts/playercontroller.cs
@@ -3,7 +3,11 @@ using UnityEngine.InputSystem;
 
 public class playercontroller : MonoBehaviour
 {
+    public float speed = 0;
+    
     private Rigidbody rb;
+    private float movementX;
+    private float movementY;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -14,10 +18,15 @@ public class playercontroller : MonoBehaviour
     void OnMove(InputValue movementValue)
     {
         Vector2 movementVector = movementValue.Get<Vector2>();
+
+        movementX = movementVector.x;
+        movementY = movementVector.y;
     }
 
     void FixedUpdate()
     {
+        Vector3 movement = new Vector3(movementX, 0.0f, movementY);
 
+        rb.AddForce(movement * speed);
     }
 }
